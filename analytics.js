88dc3b82b4a050612875eaa88ada5288c4033852
govixtech-art <govix.tech@gmail.com>
2025-12-19 10:01:51 -0300

    Add files via upload

---
+++ analytics.js
@@ -0,0 +1,7 @@
+(function () {
+  var s = document.createElement('script');
+  s.src = 'https://plausible.io/js/script.js';
+  s.defer = true;
+  s.setAttribute('data-domain', 'govix.tech');
+  document.head.appendChild(s);
+})();
